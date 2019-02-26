705f83bc7512b75599986b886764d153282bcc1e
Adam Hosman <github@adamsinbox.com>
2019-02-26 21:48:13 +0100

    Reposition logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,10 @@ import logo from './Logo.png';
 const Logo = () => {
   return (
       <div className='ma4 mt0'>
-        <Tilt className="Tilt" options={{ max: 55 }} style={{ height: 100, width: 100 }} >
-          <div className="Tilt-inner pa3"><img src={logo} alt="Logo depicting face with magic wand"/></div>
+        <Tilt className="Tilt br3" options={{ max: 55 }} style={{ height: 100, width: 100 }}>
+          <div className="Tilt-inner">
+            <img src={logo} alt="Logo depicting face with magic wand"/>
+          </div>
         </Tilt>
       </div>
   );
