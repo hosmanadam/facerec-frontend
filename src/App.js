dba331a3106a374c3ae8184d55276b6d82750601
Adam Hosman <github@adamsinbox.com>
2019-02-26 16:25:27 +0100

    Add Clarifai API call
    - Gets URL from input field
    - For now, it logs results to console

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
+
+const app = new Clarifai.App({apiKey: '0a6e9572400640b5ae84ff587db1436c'});
 
 const particlesOptions = {
   particles: {
@@ -21,6 +24,31 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  };
+
+  onButtonSubmit = (event) => {
+    app.models.predict('a403429f2ddf4b49b307e318f00e528b', this.state.input)
+        .then(this.logBoxes)
+        .catch(console.log);
+    this.setState({input: ''});
+  };
+
+  logBoxes = (response) => {
+    let regions = response.outputs[0].data.regions;
+    for (let region of regions) {
+      console.log(region.region_info.bounding_box)
+    }
+  };
+
   render() {
     return (
       <div className="App">
@@ -30,7 +58,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+            inputValue={this.state.input} />
         {/*<FaceRecognition />*/}
       </div>
     );
