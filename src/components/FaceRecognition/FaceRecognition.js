0ae915c26174da4e5f1e296fcad6e87d56e54f92
Adam Hosman <github@adamsinbox.com>
2019-02-27 17:11:38 +0100

    Update FaceRecognition component
    - Add faceBoxComponents (FaceBox component array)
    - Add foundAnnouncement

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './FaceRecognition.css'
+import FaceBox from '../FaceBox/FaceBox'
 
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, handleImage, faceBoxes, imageWidth, imageHeight, isAnalysisDone}) => {
+  let faceBoxComponents = faceBoxes.map(
+      (coordinates, i) => (
+          <FaceBox
+            key={i}
+            coordinates={coordinates}
+            imageWidth={imageWidth}
+            imageHeight={imageHeight}
+          />
+      )
+  );
+
+  let foundAnnouncement = (
+        <p className='found-announcement f3'>
+          {
+            (imageUrl === '') ? ' ' :
+            (isAnalysisDone === false) ? 'Looking for faces...' :
+            (faceBoxes.length === 0) ? 'No faces found!' :
+            (faceBoxes.length === 1) ? '1 face found!' :
+            `${faceBoxes.length} faces found!`
+          }
+        </p>
+  );
+
   return (
-      <div className='center ma'>
-        <div className='absolute mt2'>
-          <img className='br3 grow' src={imageUrl} alt=""/>
+      <div className='result-container center ma'>
+        {foundAnnouncement}
+        <div className='input-image-container mt2 relative'>
+          <img className='br3 input-image' src={imageUrl} alt="" onLoad={handleImage}/>
+          {faceBoxComponents}
         </div>
       </div>
   );
